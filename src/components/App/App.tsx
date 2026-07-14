1cb324c3c159f75faeff35093f8e5042948d925e
Myroslava Morhental <loyaslavka@gmail.com>
2026-07-14 17:35:58 +0300

    Add correction in App file

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,15 @@
+import css from "./App.module.css";
 import { useState } from "react";
 import toast, { Toaster } from "react-hot-toast";
 
 import "./App.css";
-import SearchBar from "./components/SearchBar/SearchBar";
-import { fetchMovies } from "./services/movieService";
-import type { Movie } from "./types/movie";
-import MovieGrid from "./components/MovieGrid/MovieGrid";
-import Loader from "./components/Loader/Loader";
-import ErrorMessage from "./components/ErrorMessage/ErrorMessage";
-import MovieModal from "./components/MovieModal/MovieModal";
+import SearchBar from "../SearchBar/SearchBar";
+import { fetchMovies } from "../../services/movieService";
+import type { Movie } from "../../types/movie";
+import MovieGrid from "../MovieGrid/MovieGrid";
+import Loader from "../Loader/Loader";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
+import MovieModal from "../MovieModal/MovieModal";
 
 function App() {
   const [movies, setMovies] = useState<Movie[]>([]);
@@ -44,7 +45,7 @@ function App() {
   };
 
   return (
-    <>
+    <div className={css.app}>
       <SearchBar onSubmit={handleSearch} />
       {movies.length > 0 && <MovieGrid movies={movies} onSelect={openModal} />}
       {isMoviesLoading && <Loader />}
@@ -53,7 +54,7 @@ function App() {
       {selectedMovie && (
         <MovieModal movie={selectedMovie} onClose={closeModal} />
       )}
-    </>
+    </div>
   );
 }
 
