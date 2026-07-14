d8be35edbeeeee73118b2c23ad90e86b08ab4651
Myroslava Morhental <loyaslavka@gmail.com>
2026-07-14 17:44:20 +0300

    Make corrections in App file

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,6 @@
 import css from "./App.module.css";
 import { useState } from "react";
 import toast, { Toaster } from "react-hot-toast";
-
-import "./App.css";
 import SearchBar from "../SearchBar/SearchBar";
 import { fetchMovies } from "../../services/movieService";
 import type { Movie } from "../../types/movie";
